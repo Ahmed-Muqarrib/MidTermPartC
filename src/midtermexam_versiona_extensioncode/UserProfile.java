17c7c534f21d7b4b79c6faefc531a576e60d5466
Ahmed-Muqarrib <134073912+Ahmed-Muqarrib@users.noreply.github.com>
2023-06-23 09:46:26 -0400

    Update UserProfile.java
    Added comment in github

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -15,6 +15,7 @@ package midtermexam_versiona_extensioncode;
  */
 public class UserProfile 
 {
+ //ADDED COMMENT FOR QUIZ
     
     private String userID;//the userID
     private String genre;// the user's preferred genre of movie
